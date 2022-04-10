039ab77d1e1c77e2a49c03e0a385cc2b6b3d4b6f
Nikhil Thota <nthota92@gmail.com>
2022-04-10 14:07:54 -0700

    changing models to be char for memory efficiency

--- packages/server/src/collection-owner/collection-owner.entity.ts
+++ packages/server/src/collection-owner/collection-owner.entity.ts
@@ -2,8 +2,8 @@ import { Table, Model, Column, DataType } from 'sequelize-typescript'
 
 @Table({ tableName: 'collection_owner' })
 export class CollectionOwner extends Model {
-  @Column({ type: DataType.TEXT })
+  @Column({ type: DataType.CHAR(66) })
   contract_hash: string
-  @Column({ type: DataType.TEXT })
+  @Column({ type: DataType.CHAR(42) })
   owner: string
 }
