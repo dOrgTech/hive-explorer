f08685c15de571f9285dc565fcaa7966cd95fe44
Cameron Hejazi <chejazi@umich.edu>
2022-04-13 23:55:51 -0500

    prettier fixes

--- packages/client/pages/rank.tsx
+++ packages/client/pages/rank.tsx
@@ -20,76 +20,72 @@ const initialState: InitialState = {
   error: ''
 }
 
-const width = 500;
-const height = 500;
+const width = 500
+const height = 500
 
 const drawImage = (d: RankData) => {
-  const d3 = (window as any).d3;
-  const floor = d.rank.length ? parseFloat(d.rank[d.rank.length - 1].score) : 0;
+  const d3 = (window as any).d3
+  const floor = d.rank.length ? parseFloat(d.rank[d.rank.length - 1].score) : 0
   const data = {
     name: '',
     children: d.rank.map(r => ({
       name: shortAddr(r.address),
-      size: (1000 * (parseFloat(r.score) - floor)) + 10
+      size: 1000 * (parseFloat(r.score) - floor) + 10
     }))
-  };
-  const diameter = width;
-  const format = Math.floor;
+  }
+  const diameter = width
+  const format = Math.floor
 
-  const pack = d3.layout.pack()
+  const pack = d3.layout
+    .pack()
     .size([diameter - 4, diameter - 4])
     .sort((a: any, b: any) => -(a.value - b.value))
-    .value((d: any) => d.size);
+    .value((d: any) => d.size)
 
-  const svg = d3.select('#svg-wrapper').append('svg')
-    .attr('width', diameter)
-    .attr('height', diameter);
+  const svg = d3.select('#svg-wrapper').append('svg').attr('width', diameter).attr('height', diameter)
 
-  const vis = svg.datum(data).selectAll('.node')
-    .data(pack.nodes)
-    .enter()
-    .append('g');
+  const vis = svg.datum(data).selectAll('.node').data(pack.nodes).enter().append('g')
 
   const titles = vis.append('title')
     .attr('x', (d: any) => d.x)
     .attr('y', (d: any) => d.y)
-    .text((d: any) => (d.children ? '' : `${d.name}: ${format(d.value)}`));
+    .text((d: any) => (d.children ? '' : `${d.name}: ${format(d.value)}`))
 
   const circles = vis.append('circle')
     .style('fill', (d: any) => {
       if (d.children) {
-        return 'transparent';
+        return 'transparent'
       }
-      return '#' + d.name.substr(2, 6);
+
+      return '#' + d.name.substr(2, 6)
     })
     .attr('cx', (d: any) => d.x)
     .attr('cy', (d: any) => d.y)
-    .attr('r', (d: any) => d.r);
+    .attr('r', (d: any) => d.r)
 }
 
 const copyImage = () => {
-  const svg = document.getElementById('svg-wrapper')?.children[0];
-  const img = new Image();
-  const serializer: any = new XMLSerializer();
-  const svgStr = serializer.serializeToString(svg);
-  const data = 'data:image/svg+xml;base64,' + window.btoa(svgStr);
+  const svg = document.getElementById('svg-wrapper')?.children[0]
+  const img = new Image()
+  const serializer: any = new XMLSerializer()
+  const svgStr = serializer.serializeToString(svg)
+  const data = 'data:image/svg+xml;base64,' + window.btoa(svgStr)
 
-  const canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
-  const context = canvas.getContext('2d');
-  img.src = data;
+  const canvas = document.createElement('canvas')
+  canvas.width = width
+  canvas.height = height
+  const context = canvas.getContext('2d')
+  img.src = data
   img.onload = function() {
-    context?.drawImage(img, 0, 0, width, height);
-    canvas.toBlob((blob) => {
+    context?.drawImage(img, 0, 0, width, height)
+    canvas.toBlob(blob => {
+      const item = new ClipboardItem({ 'image/png': blob as ClipboardItemDataType })
+      navigator.clipboard.write([item])
+    })
+  }
+}
 
-      const item = new ClipboardItem({ 'image/png': blob as ClipboardItemDataType });
-      navigator.clipboard.write([item]);
-    });
-  };
-};
-
-const shortAddr = (address: string) => address.substr(0, 8);
+const shortAddr = (address: string) => address.substr(0, 8)
 
 const Rank: NextPage = () => {
   const [loading, setLoading] = useState<InitialState['loading']>(initialState.loading)
@@ -100,7 +96,7 @@ const Rank: NextPage = () => {
   const resetStateForRequest = () => {
     setRankData(initialState.rankData)
     setError(initialState.error)
-    document.getElementById('svg-wrapper')!.innerHTML = '';
+    document.getElementById('svg-wrapper')!.innerHTML = ''
   }
 
   const handleSetAddress = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -159,7 +155,9 @@ const Rank: NextPage = () => {
         {error ? <h5>{error}</h5> : null}
         {rankData ? (
           <>
-            <div><button onClick={copyImage}>copy image</button></div>
+            <div>
+              <button onClick={copyImage}>copy image</button>
+            </div>
             <h5>Rank Data</h5>
             <pre>
               <code>{JSON.stringify(rankData, null, 2)}</code>
